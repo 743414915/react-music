151b196bcb43e2957954d1068aaae4d2d07f6dd8
wang <743414915@qq.com>
2024-07-25 22:12:41 +0800

    init

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CracoLessPlugin = require("craco-less");
+const path = require("path");
+
+module.exports = {
+  plugins: [
+    {   // 修改antd主题
+      plugin: CracoLessPlugin,
+    },
+
+  ],
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }
+}
