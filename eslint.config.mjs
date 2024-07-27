90b8155505f647425fe27fe754a226ead4e42045
wang <743414915@qq.com>
2024-07-27 16:23:16 +0800

    fea：集成store

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginReact, { rules } from "eslint-plugin-react";
 
 
 const eslintConfig = [
@@ -11,5 +11,9 @@ const eslintConfig = [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
+
+module.exports = {
+  rules: { "@typescript-eslint/no-explicit-any": "off", }
+}
 
 export default eslintConfig;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React, { Suspense } from "react";
-import routes from "@/router";
-
 import { useRoutes, Link } from "react-router-dom";
 
+import routes from "@/router";
+import { useAppSelector, useAppDispatch, appShallowEqual } from "@/store";
+import { changeCountAction } from "@/store/modules/counter";
+
 function App() {
+  const { count } = useAppSelector(
+    (state) => ({
+      count: state.counter.count,
+    }),
+    appShallowEqual,
+  );
+
+  const dispatch = useAppDispatch();
+  function handleChangeCount() {
+    dispatch(changeCountAction(1));
+  }
+
   return (
     <div className="App">
       <div className="nav">
@@ -12,6 +26,8 @@ function App() {
         <Link to={"/download"}>下载客户端</Link>
         <Link to={"/focus"}>关注</Link>
       </div>
+      <h2>当前计数{count}</h2>
+      <button onClick={handleChangeCount}>修改count</button>
       <Suspense fallback={<div>loading...</div>}>
         <div className="main">{useRoutes(routes)}</div>
       </Suspense>
