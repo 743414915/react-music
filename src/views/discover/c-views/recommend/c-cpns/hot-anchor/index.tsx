e1405f7141f77d554c346f92460dd7ca313ddff7
wang <743414915@qq.com>
2024-08-04 13:05:19 +0800

    fea：热门主播

--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
@@ -2,11 +2,32 @@ import React, { memo } from "react";
 
 import type { FC, PropsWithChildren } from "react";
 import { HotAnchorWrapper } from "./style";
+import AreaHeaderV2 from "@/components/area-header-v2";
+import { hotRadios } from "@/assets/data/local_data";
 
 interface IProps {}
 
 const HoyAnchor: FC<PropsWithChildren<IProps>> = () => {
-  return <HotAnchorWrapper>HoyAnchor</HotAnchorWrapper>;
+  return (
+    <HotAnchorWrapper>
+      <AreaHeaderV2 title="热门主播" />
+      <div className="anchors">
+        {hotRadios.map((item) => {
+          return (
+            <div className="item">
+              <a href="#/" className="image">
+                <img src={item.picUrl} alt="" />
+              </a>
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="desc">{item.position}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </HotAnchorWrapper>
+  );
 };
 
 export default memo(HoyAnchor);
